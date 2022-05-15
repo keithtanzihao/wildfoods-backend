7abf5e40c0904a1708224fbbf26f6c7c513f6233
Keith Tan <keithtanzihao@gmail.com>
2022-05-15 17:08:50 +0800

    routes: Api endpoints to access tables in adminpage

---
+++ routes/media.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+
+const { Media, Product } = require("../models");
+const { createMediaForm, addScssValidations } = require("../utility/forms");
+const { checkIfAuthenticated } = require("../utility/");
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+router.get("/", async (req, res) => {
+  const media = await Media.collection().fetch();
+  res.render("media/index", {
+    media: media.toJSON(),
+  });
+});
+
+router.get("/create", async (req, res) => {
+  const allProduct = await Product.fetchAll().map((product) => {
+    return [product.get("id"), product.get("title")];
+  });
+
+  const mediaForm = createMediaForm(allProduct);
+  res.render("media/create", {
+    form: mediaForm.toHTML(addScssValidations),
+    cloudinaryName: process.env.CLOUDINARY_NAME,
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinaryPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
+    uploadCareKey: process.env.UPLOADCARE_KEY
+  });
+});
+
+router.post("/create",  async (req, res) => {
+  const allProduct = await Product.fetchAll().map((product) => {
+    return [product.get("id"), product.get("title")];
+  });
+
+  const mediaForm = createMediaForm(allProduct);
+  mediaForm.handle(req, {
+    success: async (form) => {
+      const media = new Media(form.data);
+      await media.save();
+      req.flash("success", [
+        {
+          message: `Successfullly added a new media`,
+        },
+      ]);
+      res.redirect("/admin/media");
+    },
+    error: async (form) => {
+      res.render("media/create", {
+        form: form.toHTML(addScssValidations),
+      });
+    },
+  });
+});
+
+router.get("/:id/update", async (req, res) => {
+  const allProduct = await Product.fetchAll().map((product) => {
+    return [product.get("id"), product.get("title")];
+  });
+
+  const media = await Media.where({
+    id: req.params.id,
+  }).fetch({
+    require: true,
+  });
+
+  const mediaForm = createMediaForm(allProduct);
+  mediaForm.set(form.data);
+
+  res.render("products/update", {
+    form: mediaForm.toHTML(allProduct),
+    product: media.toJSON(),
+    cloudinaryName: process.env.CLOUDINARY_NAME,
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinaryPreset: process.env.CLOUDINARY_UPLOAD_PRESET,
+  });
+});
+
+module.exports = router;
---
+++ routes/staff.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const crypto = require("crypto");
+
+const router = express.Router();
+
+const { Staff } = require("../models");
+const { createLoginForm, addScssValidations } = require("../utility/forms");
+const { getHashedPassword, checkIfLogin } = require("../utility");
+
+// Register routes
+router.get("/register", checkIfLogin, async (req, res) => {
+  const staffForm = createLoginForm();
+  res.render("staff/register", {
+    form: staffForm.toHTML(addScssValidations),
+  });
+});
+
+router.post("/register", checkIfLogin, async (req, res) => {
+  const category = await Staff.query({
+    where: {
+      email: req.body.email,
+    },
+  }).fetch({
+    require: false,
+  });
+  const staffForm = createLoginForm();
+
+  if (category) {
+    req.flash("error", [
+      {
+        message: `Error: Account with email already exist`,
+      },
+    ]);
+    res.redirect("/admin/register");
+  } else {
+    staffForm.handle(req, {
+      success: async (form) => {
+        const staff = new Staff({
+          email: form.data.email,
+          password: getHashedPassword(form.data.password),
+        });
+        await staff.save();
+        req.flash("success", [
+          {
+            message: `Successfullly create a new staff`,
+          },
+        ]);
+        res.redirect("/admin/login");
+      },
+      error: async (form) => {
+        res.render("staff/register", {
+          form: form.toHTML(addScssValidations),
+        });
+      },
+    });
+  }
+});
+
+// Login routes
+router.get("/login", checkIfLogin, async (req, res) => {
+  const staffForm = createLoginForm();
+  res.render("staff/login", {
+    form: staffForm.toHTML(addScssValidations),
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const staffForm = createLoginForm();
+  staffForm.handle(req, {
+    success: async (form) => {
+      let staff = await Staff.where({
+        email: form.data.email,
+      }).fetch({
+        require: false,
+      });
+
+      if (!staff) {
+        req.flash("error", [
+          {
+            message: `Error: Wrong email/password`,
+          },
+        ]);
+        res.redirect("/admin/login");
+      } else {
+        if (staff.get("password") === getHashedPassword(form.data.password)) {
+          req.session.staff = {
+            id: staff.get("id"),
+            email: staff.get("email"),
+          };
+
+          req.flash("success", [
+            {
+              message: `Welome back ${staff.get("email")}`,
+            },
+          ]);
+          // Seems req.session.save is required to save the data into sesion-file-store
+          req.session.save(function (err) {
+            res.redirect("/admin/product");
+          });
+        } else {
+          req.flash("error", [
+            {
+              message: `Error: Wrong email/password`,
+            },
+          ]);
+          res.redirect("/admin/login");
+        }
+      }
+    },
+    error: async (form) => {
+      res.render("staff/login", {
+        form: form.toHTML(addScssValidations),
+      });
+    },
+  });
+});
+
+router.get('/logout', (req, res) => {
+  req.session.staff = null;
+  req.flash('success_messages', "Goodbye");
+  res.redirect('/admin/login');
+})
+
+
+module.exports = router;
